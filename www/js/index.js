6266b4e9c512c859ae39fbd9cdeebe83cc7f13be
Gian Matozo Ferrari <gianmatozoferrari@outlook.com.br>
2021-03-23 20:40:11 -0300

    commit required

---
+++ www/js/index.js
@@ -0,0 +1,41 @@
+function testar() {
+    var num = Number(window.document.getElementById("primo").value);
+    var res = window.document.getElementById("resprimo");
+    var divisores = 0;
+    var num1 = Number(window.document.getElementById("num1").value);
+    var num2 = Number(window.document.getElementById("num2").value);
+    var res1 = window.document.getElementById("resultado");
+    for (var count = 1; count <= num; count++)
+        if (num % count == 0) {
+            divisores++;
+        }
+    if (divisores == 2) {
+        res.innerHTML = 'O primeiro número é primo';
+    } else {
+        res.innerHTML = 'O primeiro número não é primo';
+    }
+    res1.innerHTML = `A soma de ${num1} + ${num2} = ${num1 + num2}` + `</br></br>` + 
+    `A subtração de ${num1} - ${num2} = ${num1 - num2}` + `</br></br>` +
+    `A divisão de ${num1} / ${num2} = ${num1 / num2}` + `</br></br>` + 
+    `A multiplicação de ${num1} x ${num2} = ${num1 * num2}` + `</br></br>` + 
+    `A sobra da divisão de ${num1} % ${num2} = ${num1 % num2}` + `</br></br>`;
+    
+}
+
+function limpa() {
+    var num = Number(window.document.getElementById("primo").value);
+    var res = window.document.getElementById("resprimo");
+    var divisores = 0;
+    var num1 = Number(window.document.getElementById("num1").value);
+    var num2 = Number(window.document.getElementById("num2").value);
+    var res1 = window.document.getElementById("resultado");
+    if (document.getElementById('primo').value != "" || document.getElementById('num2').value != "" || document.getElementById('num1').value != "") {
+        document.getElementById('primo').value = "";
+        document.getElementById('num1').value = "";
+        document.getElementById('num2').value = "";
+    }
+    if(document.getElementById('resprimo').value != "" || document.getElementById('resultado').value != ""){
+        res.innerHTML = "</br>";
+        res1.innerHTML = "</br>";
+    }
+}
